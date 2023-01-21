3aa544ac384c1a509f7a1ceb0f696ae280baaab7
zeldacod5 <loucas.herault@laposte.net>
2023-01-21 04:07:46 +0100

    page produit + redirection

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Product;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -44,5 +46,7 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class);
+        yield MenuItem::linkToCrud('Products', 'fas fa-tag', Product::class);
     }
 }
---
+++ src/Controller/ProductController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProductController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    #[Route('/nos-produits', name: 'products')]
+    public function index(): Response
+    {
+
+        $products = $this->entityManager->getRepository(Product::class)->findAll();
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+    #[Route('/produit/{slug}', name: 'product')]
+    public function show($slug): Response
+    {
+
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+
+        if(!$product) {
+            return $this->redirectToRoute('products');
+        }
+        return $this->render('product/show.html.twig', [
+            'product' => $product
+        ]);
+    }
+
+}
